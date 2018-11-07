c87652885c7e092f217c1e9625360c9e207d613d
yyxxzj <381634002@qq.com>
2018-11-07 16:56:50 +0800

    test

---
+++ src/main/java/test/Log1.java
@@ -0,0 +1,10 @@
+package test;
+
+public class Log1 implements Log {
+
+	public void execute() {
+		System.out.println("log2--------------");
+
+	}
+
+}
--- src/main/java/test/test.java
+++ src/main/java/test/test.java
@@ -1,5 +1,15 @@
 package test;
 
-public class test {
+import java.util.Iterator;
+import java.util.ServiceLoader;
 
+public class test {
+	public static void main(String[] args) {
+        ServiceLoader<Log> serviceLoader = ServiceLoader.load(Log.class);
+       Iterator<Log> iterator = serviceLoader.iterator();
+       while (iterator.hasNext()) {
+           Log log = iterator.next();
+           log.execute();
+       }
+   }
 }
