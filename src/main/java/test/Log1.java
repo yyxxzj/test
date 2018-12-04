e39d070a244796f8cc93aa92cdb7a2f601bb05cd
Administrator <Administrator@PC-20181123OGOL>
2018-12-04 23:31:49 +0800

    ddd

--- src/main/java/test/Log1.java
+++ src/main/java/test/Log1.java
@@ -3,7 +3,9 @@ package test;
 public class Log1 implements Log {
 
 	public void execute() {
-		System.out.println("log1ccc-------------");
+		System.out.println("log1-------------");
+
+		System.out.println("log1222-------------");
 		System.out.println("test switch branch....");
 	}
 
