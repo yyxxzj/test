86e1086b2472b5d95bb1a202fa58151a74d80f3f
Administrator <Administrator@PC-20181123OGOL>
2018-12-04 23:03:36 +0800

    add new file

--- src/main/java/test/Log1.java
+++ src/main/java/test/Log1.java
@@ -3,7 +3,7 @@ package test;
 public class Log1 implements Log {
 
 	public void execute() {
-		System.out.println("log2--------------");
+		System.out.println("log1-------------");
 
 	}
 
---
+++ src/main/java/test/Log3.java
@@ -0,0 +1,10 @@
+package test;
+
+public class Log3 implements Log {
+
+	public void execute() {
+		System.out.println("log3...........");
+
+	}
+
+}
